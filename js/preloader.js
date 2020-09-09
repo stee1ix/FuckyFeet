b1bea21a7125163b0b3eb9bffe9ea8665b35673e
Rituraj Ranjan <rituraj.ranjan.42@gmail.com>
2020-09-09 17:36:23 +0530

    preloader

---
+++ js/preloader.js
@@ -0,0 +1,12 @@
+<script>
+        $(document).ready(function () {
+            //Preloader
+            preloaderFadeOutTime = 500;
+
+            function hidePreloader() {
+                var preloader = $('.spinner-wrapper');
+                preloader.fadeOut(preloaderFadeOutTime);
+            }
+            hidePreloader();
+        });
+    </script>
